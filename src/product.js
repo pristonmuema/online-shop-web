9c9da584f9b9cf4a7f1e56108ecf409934bdac0d
Priston Muema <35369901+pristonmuema@users.noreply.github.com>
2023-08-09 15:36:34 +0300

    Online Shop Project

---
+++ src/product.js
@@ -0,0 +1,50 @@
+import product1 from "./assets/products/Mac.jpeg"
+import product2 from "./assets/products/HP.jpeg"
+import product3 from "./assets/products/Dell.jpg"
+import product4 from "./assets/products/Asus Tuff.jpeg"
+import product5 from "./assets/products/Lenovo.jpeg"
+import product6 from "./assets/products/mac air.jpeg"
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Mac Book Pro",
+    price: 3000,
+    currency: "USD",
+    productImage: product1
+  },
+  {
+    id: 2,
+    productName: "HP",
+    price: 2300,
+    currency: "USD",
+    productImage: product2
+  },
+  {
+    id: 3,
+    productName: "Dell",
+    price: 2400,
+    currency: "USD",
+    productImage: product3
+  },
+  {
+    id: 4,
+    productName: "Asus Tuf",
+    price: 2100,
+    currency: "USD",
+    productImage: product4
+  },
+  {
+    id: 5,
+    productName: "Lenovo",
+    price: 2400,
+    currency: "USD",
+    productImage: product5
+  },
+  {
+    id: 6,
+    productName: "Mac Book Air",
+    price: 2100,
+    currency: "USD",
+    productImage: product6
+  }
+]
